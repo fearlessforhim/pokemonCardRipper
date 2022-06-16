3f0e6345a4d177ed2f909c317448bb56af698390
Jonathan Cameron <jonathancameron6@protonmail.com>
2022-06-16 15:41:13 -0700

    Read from file, print periods for progress

--- rip.py
+++ rip.py
@@ -2,17 +2,17 @@ import sys
 import requests
 import json
 
-print(sys.argv)
-response = requests.get("https://api.pokemontcg.io/v2/cards?q=number:{num}%20set.printedTotal:{printTotal}".format(num=sys.argv[1], printTotal=sys.argv[2]))
-obj = json.loads(response.text)["data"][0]
-print(obj["name"])
-print(obj["types"][0])
-print(obj["nationalPokedexNumbers"][0])
 
-csvVal = "{num}/{printTotal},{natNumber},{name},{type}".format(num=sys.argv[1], printTotal=sys.argv[2], natNumber=obj["nationalPokedexNumbers"][0], name=obj["name"], type=obj["types"][0])
+f = open("numbers.txt", "r")
+for x in f:
+  line = x.replace("\n", "")
 
-print(csvVal)
+  response = requests.get("https://api.pokemontcg.io/v2/cards?q=number:{num}%20set.printedTotal:{printTotal}".format(num=line.split("/")[0], printTotal=line.split("/")[1]))
+  obj = json.loads(response.text)["data"][0]
 
-file1 = open("pokemon.csv", "a")  # append mode
-file1.write(csvVal)
-file1.close()
+  csvVal = "{num}/{printTotal},{natNumber},{name},{type}\n".format(num=line.split("/")[0], printTotal=line.split("/")[1], natNumber=obj["nationalPokedexNumbers"][0], name=obj["name"], type=obj["types"][0])
+
+  file1 = open("pokemon.csv", "a")  # append mode
+  file1.write(csvVal)
+  file1.close()
+  print(".", end="", flush=True)
