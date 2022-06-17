ab79661ee42506a15827384c70ef43553f9abf79
Jonathan Cameron <jonathancameron6@protonmail.com>
2022-06-16 21:01:32 -0700

    Create specific source file, update executable to handle file entries and log only

--- rip.py
+++ rip.py
@@ -3,14 +3,32 @@ import requests
 import json
 import time
 
-f = open("numbers.txt", "r")
+f = open(sys.argv[1], "r")
+logOnly = False
+
+try:
+    logOnly = sys.argv[2] == "logOnly"
+except:
+    pass
+
 for x in f:
     line = x.replace("\n", "")
     print(line)
 
     try:
-        response = requests.get("https://api.pokemontcg.io/v2/cards?q=number:{num}%20set.printedTotal:{printTotal}".format(num=line.split("/")[0], printTotal=line.split("/")[1]))
-        obj = json.loads(response.text)["data"][0]
+        response = None
+        num = line.split("/")[0]
+        printTotal = line.split("/")[1]
+        if len(line.split("/")) > 2:
+            response = requests.get("https://api.pokemontcg.io/v2/cards?q=number:{num}%20set.printedTotal:{printTotal}%20name:{name}".format(num=num, printTotal=printTotal, name=line.split("/")[2]))
+        else:
+            response = requests.get("https://api.pokemontcg.io/v2/cards?q=number:{num}%20set.printedTotal:{printTotal}".format(num=num, printTotal=printTotal))
+
+        raw = json.loads(response.text)["data"]
+        if len(raw) > 1:
+            print("multiple!")
+            continue
+        obj = raw[0]
 
         normalPrice = 0
         holoFoilPrice = 0
@@ -27,9 +45,12 @@ for x in f:
 
         csvVal = "{num},{printTotal},{natNumber},{name},{type},{normalPrice},{reverseHoloFoilPrice},{holoFoilPrice},{priceUpdated}\n".format(num=line.split("/")[0], printTotal=line.split("/")[1], natNumber=obj["nationalPokedexNumbers"][0], name=obj["name"], type=obj["types"][0], normalPrice=normalPrice, reverseHoloFoilPrice=reverseHoloFoilPrice, holoFoilPrice=holoFoilPrice, priceUpdated=obj["tcgplayer"]["updatedAt"])
 
-        file1 = open("pokemon.csv", "a")  # append mode
-        file1.write(csvVal)
-        file1.close()
+        if logOnly:
+            print(csvVal)
+        else:
+            file1 = open("pokemon.csv", "a")  # append mode
+            file1.write(csvVal)
+            file1.close()
     except:
         print("Failed!")
         csvVal = "{num},{printTotal},???,???,???,???,???,???,???\n".format(num=line.split("/")[0], printTotal=line.split("/")[1])
